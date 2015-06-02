f03966f3b6e8dd8e5dd0ab75c614bd35b0056524
Lucas Schirm <lucasschirm@gmail.com>
2015-06-02 14:29:33 -0300

    V 0.1
    Go on every directory and make a clone with only changed files on the current day.

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$dir = dirname(__FILE__) . "/";
+$patch = dirname(__FILE__) . "/patch/";
+
+
+cp($dir, "", $patch);
+
+function cp($dir, $file, $patch) {
+
+    if ($file == "patch" || $file == "index.php")
+        return;
+
+    if (is_dir($dir . $file)) {
+        $patch = $patch . $file . "/";
+        $dir = $dir . $file . "/";
+        $files = scandir($dir);
+        
+        foreach ($files as $file) {
+            if ($file != "." && $file != "..") {
+                cp($dir, $file, $patch);
+            }
+        }
+    } else {
+        $t = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
+
+
+        if (filemtime($dir . $file) > $t) {
+            if (!file_exists($patch))
+                mkdir($patch, 0777, true);
+            
+            copy($dir . $file, $patch . $file);
+        }
+    }
+}
